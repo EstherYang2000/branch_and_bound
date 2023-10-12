1d19db8945e7551306bf1242e8661b266520f5b7
esther <ester6126@gmail.com>
2023-10-12 10:14:22 +0800

    second version

--- full_enumeration_tree.py
+++ full_enumeration_tree.py
@@ -3,18 +3,16 @@
 # • Q3: What is the difference between the optimal value of Q2 and the optimal value of Q1.
 # • Q4: By the way, what is the run time of the SRPT algorithm?
 
-# jobj 1 2 3 4 5 6
-# rj   0 2 2 6 7 9
+# jobj 1 2 3 4 5 6 
+# rj   0 2 2 6 7 9 
 # pj   6 2 5 2 8 2
 
-# 0 1 3 5 2 4
-# 6 11 19 21
+#0 1 3 5 2 4 
+# 6 11 19 21 
 
 import itertools
-import heapq
 
-
-def completion_time(jobs: int, permutation: list):
+def completion_time(jobs:int, permutation:list):
     completion_times = []
     current_time = 0
     for idx in permutation:
@@ -25,29 +23,94 @@ def completion_time(jobs: int, permutation: list):
             return 0
     return completion_times[-1]
 
-
-def full_enumeration_tree(jobs: list):
+def full_enumeration_tree(jobs:list):
     all_permutations_list = list(itertools.permutations(range(len(jobs))))
     best_time_list = []
-    for idx, permutation in enumerate(all_permutations_list):
+    for idx,permutation in enumerate(all_permutations_list):
         objective_value = completion_time(jobs, permutation)
-        if objective_value != 0:
-            best_time_list.append((idx, objective_value))
-            print(
-                f"Permutation: {permutation}, Total Completion Time: {objective_value}"
-            )
+        if objective_value != 0 :
+            best_time_list.append((idx,objective_value))
+            print(f"Permutation: {permutation}, Total Completion Time: {objective_value}")
     print(best_time_list)
 
+    
+if __name__ == '__main__':
 
-if __name__ == "__main__":
     jobs = [(0, 6), (2, 2), (2, 5), (6, 2), (7, 8), (9, 2)]
     full_enumeration_tree(jobs)
-
-    # jobj 1 2 3 4 5 6
-    # rj   0 2 2 6 7 9
+    
+    # jobj 1 2 3 4 5 6 
+    # rj   0 2 2 6 7 9 
     # pj   6 2 5 2 8 2
 
 
-# max_index = best_time_list.index(min(best_time_list[1:]))
-# print("Best permuttation:"+str(all_permutations[max_index]))
-# print("Min_processing_time:"+str(min(best_time_list)))
+    
+    """ Q1
+    Permutation: (0, 1, 2, 3, 4, 5), Total Completion Time: 25
+    Permutation: (0, 1, 2, 3, 5, 4), Total Completion Time: 25
+    Permutation: (0, 1, 2, 4, 3, 5), Total Completion Time: 25
+    Permutation: (0, 1, 2, 4, 5, 3), Total Completion Time: 25
+    Permutation: (0, 1, 2, 5, 3, 4), Total Completion Time: 25
+    Permutation: (0, 1, 2, 5, 4, 3), Total Completion Time: 25
+    Permutation: (0, 1, 3, 2, 4, 5), Total Completion Time: 25
+    Permutation: (0, 1, 3, 2, 5, 4), Total Completion Time: 25
+    Permutation: (0, 1, 3, 4, 2, 5), Total Completion Time: 25
+    Permutation: (0, 1, 3, 4, 5, 2), Total Completion Time: 25
+    Permutation: (0, 1, 3, 5, 2, 4), Total Completion Time: 25
+    Permutation: (0, 1, 3, 5, 4, 2), Total Completion Time: 25
+    Permutation: (0, 1, 4, 2, 3, 5), Total Completion Time: 25
+    Permutation: (0, 1, 4, 2, 5, 3), Total Completion Time: 25
+    Permutation: (0, 1, 4, 3, 2, 5), Total Completion Time: 25
+    Permutation: (0, 1, 4, 3, 5, 2), Total Completion Time: 25
+    Permutation: (0, 1, 4, 5, 2, 3), Total Completion Time: 25
+    Permutation: (0, 1, 4, 5, 3, 2), Total Completion Time: 25
+    Permutation: (0, 2, 1, 3, 4, 5), Total Completion Time: 25
+    Permutation: (0, 2, 1, 3, 5, 4), Total Completion Time: 25
+    Permutation: (0, 2, 1, 4, 3, 5), Total Completion Time: 25
+    Permutation: (0, 2, 1, 4, 5, 3), Total Completion Time: 25
+    Permutation: (0, 2, 1, 5, 3, 4), Total Completion Time: 25
+    Permutation: (0, 2, 1, 5, 4, 3), Total Completion Time: 25
+    Permutation: (0, 2, 3, 1, 4, 5), Total Completion Time: 25
+    Permutation: (0, 2, 3, 1, 5, 4), Total Completion Time: 25
+    Permutation: (0, 2, 3, 4, 1, 5), Total Completion Time: 25
+    Permutation: (0, 2, 3, 4, 5, 1), Total Completion Time: 25
+    Permutation: (0, 2, 3, 5, 1, 4), Total Completion Time: 25
+    Permutation: (0, 2, 3, 5, 4, 1), Total Completion Time: 25
+    Permutation: (0, 2, 4, 1, 3, 5), Total Completion Time: 25
+    Permutation: (0, 2, 4, 1, 5, 3), Total Completion Time: 25
+    Permutation: (0, 2, 4, 3, 1, 5), Total Completion Time: 25
+    Permutation: (0, 2, 4, 3, 5, 1), Total Completion Time: 25
+    Permutation: (0, 2, 4, 5, 1, 3), Total Completion Time: 25
+    Permutation: (0, 2, 4, 5, 3, 1), Total Completion Time: 25
+    Permutation: (0, 2, 5, 1, 3, 4), Total Completion Time: 25
+    Permutation: (0, 2, 5, 1, 4, 3), Total Completion Time: 25
+    Permutation: (0, 2, 5, 3, 1, 4), Total Completion Time: 25
+    Permutation: (0, 2, 5, 3, 4, 1), Total Completion Time: 25
+    Permutation: (0, 2, 5, 4, 1, 3), Total Completion Time: 25
+    Permutation: (0, 2, 5, 4, 3, 1), Total Completion Time: 25
+    Permutation: (0, 3, 1, 2, 4, 5), Total Completion Time: 25
+    Permutation: (0, 3, 1, 2, 5, 4), Total Completion Time: 25
+    Permutation: (0, 3, 1, 4, 2, 5), Total Completion Time: 25
+    Permutation: (0, 3, 1, 4, 5, 2), Total Completion Time: 25
+    Permutation: (0, 3, 1, 5, 2, 4), Total Completion Time: 25
+    Permutation: (0, 3, 1, 5, 4, 2), Total Completion Time: 25
+    Permutation: (0, 3, 2, 1, 4, 5), Total Completion Time: 25
+    Permutation: (0, 3, 2, 1, 5, 4), Total Completion Time: 25
+    Permutation: (0, 3, 2, 4, 1, 5), Total Completion Time: 25
+    Permutation: (0, 3, 2, 4, 5, 1), Total Completion Time: 25
+    Permutation: (0, 3, 2, 5, 1, 4), Total Completion Time: 25
+    Permutation: (0, 3, 2, 5, 4, 1), Total Completion Time: 25
+    Permutation: (0, 3, 4, 1, 2, 5), Total Completion Time: 25
+    Permutation: (0, 3, 4, 1, 5, 2), Total Completion Time: 25
+    Permutation: (0, 3, 4, 2, 1, 5), Total Completion Time: 25
+    Permutation: (0, 3, 4, 2, 5, 1), Total Completion Time: 25
+    Permutation: (0, 3, 4, 5, 1, 2), Total Completion Time: 25
+    Permutation: (0, 3, 4, 5, 2, 1), Total Completion Time: 25
+    
+    
+    
+    """
+
+
+
+
